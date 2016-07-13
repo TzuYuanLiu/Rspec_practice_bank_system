5be5513fc5274cc313768193e5cbc580c307f6cf
TzuYuan <tzuyuanliu0830@gmail.com>
2016-07-13 10:20:13 +0800

    add basic deposit and withdraw money methods

--- app/models/bank_account.rb
+++ app/models/bank_account.rb
@@ -1,2 +1,14 @@
 class BankAccount < ActiveRecord::Base
+	def 存錢(n)
+		update_attributes(amount: self.amount + n) if n.positive?
+	end
+	
+	def 餘額
+		amount
+	end	
+
+	def 領錢(n)
+		update_attributes(amount: self.amount - n) if n.positive?
+	end
+
 end
--- spec/models/bank_account_spec.rb
+++ spec/models/bank_account_spec.rb
@@ -1,4 +1,28 @@
 require 'rails_helper'
 
 RSpec.describe BankAccount, type: :model do
+	it "可以存錢" do
+		account = BankAccount.create(name: "foo", amount: 100)
+		account.存錢(100)
+		expect(account.餘額).to be 200
+	end
+
+	it "不可以存負的錢" do
+		account = BankAccount.create(name: "foo", amount: 100)
+		account.存錢(-10)
+		expect(account.餘額).to be 100
+	end
+
+	it "可以領錢" do
+		account = BankAccount.create(name: "foo", amount: 100)
+		account.領錢(10)
+		expect(account.餘額).to be 90
+	end
+
+	it "不可以領負的錢" do
+		account = BankAccount.create(name: "foo", amount: 100)
+		account.領錢(-10)
+		expect(account.餘額).to be 100
+	end
+
 end
