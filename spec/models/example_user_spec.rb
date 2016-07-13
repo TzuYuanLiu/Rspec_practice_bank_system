2c58b989e54f1305165b05edab7c32e55fa006e6
Eddie Kao <eddie@digik.com.tw>
2016-07-13 09:26:54 +0800

    remove unused file

--- spec/models/example_user_spec.rb
+++
@@ -1,8 +0,0 @@
-require 'rails_helper'
-
-describe ExampleUser, type: :model do
-  it "add a new example user" do
-    user = create(:new_example_user)
-    expect(ExampleUser.count).to be 1
-  end
-end
